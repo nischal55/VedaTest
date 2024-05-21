5a6fb69c4ab7286226136e98cdd34e9e5689a47b
Nripesh <nripeshparajuli2@gmail.com>
2024-05-21 17:35:58 +0545

    new change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
+
+<div class="container">
     <div class="row">
         <div class="col-md-8">
-            <h1>Expenses Calculator</h1>
-            <!-- Your calculator content here -->
-            <p>This is a static page about the expenses calculator.</p>
+            <div class="card mb-3">
+                <div class="card-body">
+                    <h1 class="card-title">Expenses Calculator</h1>
+                    <!-- Your calculator content here -->
+                    <p class="card-text">This is a static page about the expenses calculator.</p>
+                </div>
+            </div>
         </div>
-        <div class="col-md-4">
-            <!-- Additional content, such as sidebar or ads, can be placed here -->
-        </div>
+      
     </div>
+</div>
+
 @endsection
